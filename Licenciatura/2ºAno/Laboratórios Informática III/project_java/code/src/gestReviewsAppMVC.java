d20aefddb54fb9745c5f4b0fddf8cba3e03d55c4
Maria Cunha <61948956+stellaechild@users.noreply.github.com>
2022-12-14 14:19:23 +0000

    Create gestReviewsAppMVC.java

---
+++ Licenciatura/2ºAno/Laboratórios Informática III/project_java/code/src/gestReviewsAppMVC.java
@@ -0,0 +1,14 @@
+/**
+ * @Author Vicente Moreira
+ * @Date 13/05/2021
+ *
+ * Classe Main
+ */
+
+public class gestReviewsAppMVC {
+
+    public static void main(String[] args) {
+        Controller c = new Controller();
+        c.run();
+    }
+}
